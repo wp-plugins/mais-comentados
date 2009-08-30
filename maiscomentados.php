e620cd188849668c7c2c835654ae7cd8bcfaad78
Zeiro <Zeiro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-30 13:51:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mais-comentados/tags/1.4@150544 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ maiscomentados.php
@@ -0,0 +1,99 @@
+﻿<?php
+/*
+Plugin Name: Mais Comentados
+Plugin URI: http://cybernerd.info/wp-plugins/mais-comentados/
+Description: Exibe o Link das publicações que têm mais comentários.
+Author: Ronis Nascimento
+Version: 1.4
+Author URI: http://cybernerd.info
+*/
+
+add_action("plugins_loaded", "init_comentados");
+
+function mais_comentados($arg) {
+	global $wpdb;
+
+$options = get_option("widget_maiscomentados");
+if (!is_array( $options ))
+	{
+		$options = array(
+      'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
+      );
+  }    
+
+//obter o post da base de dados
+$posts = $wpdb->get_results("SELECT ID, post_title, comment_count FROM $wpdb->posts ORDER BY comment_count DESC LIMIT " . $options['number'] . "");
+
+//determinar se utilizada como uma barra lateral ou função
+if ($arg == '')
+	echo '<li>';
+?>
+<h2><?php echo $options['title']; ?></h2>
+<ul>
+<?php
+//mostrar cada página como um link
+foreach ($posts as $links) {
+	if ($options['comment'] == 'checked')
+		$comments = ' (' . $links->comment_count . ')';
+
+	echo '<li><a href="' . get_permalink($links->ID) . '">' . $links->post_title . '</a>' . $comments . '</li>';
+	}
+?>
+
+</ul>
+<?php
+if ($arg == '')
+	echo '</li>';
+?>
+
+<?php
+}
+
+function init_comentados(){
+	//inicializar a widget
+    register_sidebar_widget("Mais Comentados", "mais_comentados");     
+	register_widget_control('Mais Comentados', 'maiscomentados_controle', 200, 300 );
+}
+
+function maiscomentados_controle() {
+	update_option("widget_maiscomentados-numero", "5");
+	$options = get_option("widget_maiscomentados");
+
+//se deixar opção em branco, inserir dados predefinidos.
+if (!is_array( $options ))
+	{
+		$options = array(
+      'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
+      );
+  }    
+
+//apresentar novos dados
+if ($_POST['maiscomentados-Enviar'])
+  {
+	$options['title'] = htmlspecialchars($_POST['maiscomentados-titulo']);
+    $options['number'] = htmlspecialchars($_POST['maiscomentados-numero']);
+	$options['comment'] = htmlspecialchars($_POST['maiscomentados-comentario']);
+	
+    update_option("widget_maiscomentados", $options);
+  }
+//cria painel de configuração do widget
+?>
+	<p>
+    <label for="maiscomentados-titulo">Titulo: </label>
+    <input type="text" id="maiscomentados-titulo" name="maiscomentados-titulo" value="<?php echo $options['title'];?>" />
+  </p>
+
+	<p>
+    <label for="maiscomentados-numero">Número de publicações a ser exibido: </label>
+    <input type="text" id="maiscomentados-numero" name="maiscomentados-numero" value="<?php echo $options['number'];?>" size="5" />
+  </p>
+
+<p>
+	 <input type="checkbox" id="maiscomentados-comentario" name="maiscomentados-comentario" value="checked" <?php echo $options['comment'];?> />
+    <label for="maiscomentados-comentario"> Exibir número de comentários.</label>
+
+	<input type="hidden" id="maiscomentados-Enviar" name="maiscomentados-Enviar" value="1" />
+  </p>
+<?php
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+﻿=== Mais Comentados ===
+Contributors: Ronis Nascimento
+Donate link: http://cybernerd.info
+Tags: widget, mais populares, comentarios, sidebar, comment, most popular
+Requires at least: 2.0
+Tested up to: 2.8.4
+Stable tag: 1.4
+
+Este é um widget muito simples, ele exibe um link para as postagens que tem mais comentários em seu blog.
+
+== Description ==
+
+Este widget é uma base para a sua sidebar, que cria uma lista de links para o início postagens no blog de acordo com o número de comentários sobre a postagem. Você pode personalizar o título do widget, o número de links para mostrar e mesmo que vocé não deseja exibir o contador de comentário.
+
+Para obter uma lista completa das mudanças de cada versão, visite <a href="http://cybernerd.info/wp-plugins/mais-comentados/">a 
+pagina do plugin</a>.
+
+== Uso ==
+
+Utilizar como qualquer um widget com o procedimento de instalação padrão, ou utilizar a própria função em um post, adicionando &lt;?php mais_comentados(1);?&gt; no template. O argumento 1 garante que a lista é auto-contida ao invez de parte da sidebar.
+
+Duvidas:
+<a href="http://www.twitter.com/supersincero/">www.twitter.com/supersincero</a>
+
+== Installation ==
+
+1. Enviar o arquivo maiscomentados.php para o diretório de plugins do wordpress.
+2. Ativar o plugin.
+3. Navegue para o Painel de Configuração e personalize o widget.
+4. Se você quiser simplesmente mostrar os dados como uma lista em um post, use &lt;?php mais_comentados(1);?&gt; em seu template.
+
+== Screenshots ==
+
+1. Painel de Configuração do Plugin. :D
+2. Exemplo de uso do plugin no blog trocistas.
+
+== Frequently Asked Questions ==
+
+É configuravel?
+
+Sim. Dê uma olhada nas opções do painel de configuração do widget.
+
+Duvidas/Reclamações/Sugestões:
+<a href="http://www.twitter.com/supersincero/">www.twitter.com/supersincero</a>
