033888ba7cea5af346a8d29b3047d7a46763dea4
reitor <reitor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 23:29:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mais-comentados/trunk@295436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- maiscomentados.php
+++ maiscomentados.php
@@ -1,99 +1,68 @@
 ﻿<?php
 /*
 Plugin Name: Mais Comentados
-Plugin URI: http://cybernerd.info/wp-plugins/mais-comentados/
-Description: Exibe o Link das publicações que têm mais comentários.
-Author: Ronis Nascimento
-Version: 1.4
-Author URI: http://cybernerd.info
+Plugin URI: http://reitor.org/wp-plugins/mais-comentados/
+Description: Exibe links das publicações que possuem mais comentários.
+Author: Ronis Reitor
+Version: 1.5
+Author URI: http://reitor.org
 */
-
-add_action("plugins_loaded", "init_comentados");
-
-function mais_comentados($arg) {
+add_action("plugins_loaded", "init_mc");
+function mais_comentados($args) {
 	global $wpdb;
-
-$options = get_option("widget_maiscomentados");
-if (!is_array( $options ))
-	{
+	$options = get_option("maiscomentados");
+	if(!is_array( $options )){
 		$options = array(
-      'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
-      );
-  }    
-
-//obter o post da base de dados
-$posts = $wpdb->get_results("SELECT ID, post_title, comment_count FROM $wpdb->posts ORDER BY comment_count DESC LIMIT " . $options['number'] . "");
-
-//determinar se utilizada como uma barra lateral ou função
-if ($arg == '')
-	echo '<li>';
+			'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
+		);
+	}
+	$posts = $wpdb->get_results("SELECT ID, post_title, comment_count FROM {$wpdb->posts} WHERE post_status='publish' AND post_type='post' ORDER BY comment_count DESC LIMIT {$options['number']}");
+	echo $args['before_widget'] . $args['before_title'] . $options['title'] . $args['after_title'];
+	echo '<ul>';
 ?>
-<h2><?php echo $options['title']; ?></h2>
-<ul>
 <?php
-//mostrar cada página como um link
-foreach ($posts as $links) {
-	if ($options['comment'] == 'checked')
-		$comments = ' (' . $links->comment_count . ')';
-
-	echo '<li><a href="' . get_permalink($links->ID) . '">' . $links->post_title . '</a>' . $comments . '</li>';
+	foreach ($posts as $links) {
+		if ($options['comment'] == 'checked'){
+			$comments = ' (' . $links->comment_count . ')';
+		}
+		echo '<li><a href="' . get_permalink($links->ID) . '">' . $links->post_title . $comments . '</a></li>'."\n";
 	}
 ?>
-
-</ul>
 <?php
-if ($arg == '')
-	echo '</li>';
-?>
-
-<?php
+	echo '</ul>';
+	echo $args['after_widget'];
 }
-
-function init_comentados(){
-	//inicializar a widget
-    register_sidebar_widget("Mais Comentados", "mais_comentados");     
-	register_widget_control('Mais Comentados', 'maiscomentados_controle', 200, 300 );
+function init_mc(){
+	register_sidebar_widget("Mais Comentados", "mais_comentados");
+	register_widget_control('Mais Comentados', 'mc_controle', 200, 300);
 }
-
-function maiscomentados_controle() {
-	update_option("widget_maiscomentados-numero", "5");
-	$options = get_option("widget_maiscomentados");
-
-//se deixar opção em branco, inserir dados predefinidos.
-if (!is_array( $options ))
-	{
+function mc_controle() {
+	$options = get_option("maiscomentados");
+	if(!is_array($options)){
 		$options = array(
-      'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
-      );
-  }    
-
-//apresentar novos dados
-if ($_POST['maiscomentados-Enviar'])
-  {
-	$options['title'] = htmlspecialchars($_POST['maiscomentados-titulo']);
-    $options['number'] = htmlspecialchars($_POST['maiscomentados-numero']);
-	$options['comment'] = htmlspecialchars($_POST['maiscomentados-comentario']);
-	
-    update_option("widget_maiscomentados", $options);
-  }
-//cria painel de configuração do widget
+			'number' => '5', 'comment' => 'checked', 'title' => 'Mais Comentados'
+		);
+	}
+	if($_POST['mc-enviar']){
+		$options['title'] = htmlspecialchars($_POST['mc-titulo']);
+		$options['number'] = htmlspecialchars($_POST['mc-numero']);
+		$options['comment'] = htmlspecialchars($_POST['mc-comentario']);
+		update_option("maiscomentados", $options);
+	}
 ?>
-	<p>
-    <label for="maiscomentados-titulo">Titulo: </label>
-    <input type="text" id="maiscomentados-titulo" name="maiscomentados-titulo" value="<?php echo $options['title'];?>" />
-  </p>
-
-	<p>
-    <label for="maiscomentados-numero">Número de publicações a ser exibido: </label>
-    <input type="text" id="maiscomentados-numero" name="maiscomentados-numero" value="<?php echo $options['number'];?>" size="5" />
-  </p>
-
 <p>
-	 <input type="checkbox" id="maiscomentados-comentario" name="maiscomentados-comentario" value="checked" <?php echo $options['comment'];?> />
-    <label for="maiscomentados-comentario"> Exibir número de comentários.</label>
-
-	<input type="hidden" id="maiscomentados-Enviar" name="maiscomentados-Enviar" value="1" />
-  </p>
+	<label for="mc-titulo">Titulo: </label>
+	<input type="text" id="mc-titulo" name="mc-titulo" value="<?php echo $options['title'];?>" />
+</p>
+<p>
+	<label for="mc-numero">Número de publicações a ser exibido: </label>
+	<input type="text" id="maiscomentados-numero" name="mc-numero" value="<?php echo $options['number'];?>" size="5" />
+</p>
+<p>
+	<input type="checkbox" id="mc-comentario" name="mc-comentario" value="checked" <?php echo $options['comment'];?> />
+	<label for="mc-comentario"> Exibir número de comentários.</label>
+	<input type="hidden" id="mc-enviar" name="mc-enviar" value="1" />
+</p>
 <?php
-	}
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -1,44 +1,32 @@
 ﻿=== Mais Comentados ===
-Contributors: Ronis Nascimento
-Donate link: http://cybernerd.info
+Contributors: reitor
+Donate link: http://reitor.org
 Tags: widget, mais populares, comentarios, sidebar, comment, most popular
 Requires at least: 2.0
-Tested up to: 2.8.4
-Stable tag: 1.4
+Tested up to: 3.0.1
+Stable tag: 1.5
 
-Este é um widget muito simples, ele exibe um link para as postagens que tem mais comentários em seu blog.
+Este é um widget muito simples, ele exibe links para postagens que possuem mais comentários.
 
 == Description ==
 
-Este widget é uma base para a sua sidebar, que cria uma lista de links para o início postagens no blog de acordo com o número de comentários sobre a postagem. Você pode personalizar o título do widget, o número de links para mostrar e mesmo que vocé não deseja exibir o contador de comentário.
+Este widget, cria em sua sidebar(barra lateral) uma lista de links para os posts que possuem mais comentários.
 
-Para obter uma lista completa das mudanças de cada versão, visite <a href="http://cybernerd.info/wp-plugins/mais-comentados/">a 
-pagina do plugin</a>.
-
-== Uso ==
-
-Utilizar como qualquer um widget com o procedimento de instalação padrão, ou utilizar a própria função em um post, adicionando &lt;?php mais_comentados(1);?&gt; no template. O argumento 1 garante que a lista é auto-contida ao invez de parte da sidebar.
-
-Duvidas:
-<a href="http://www.twitter.com/supersincero/">www.twitter.com/supersincero</a>
+Para obter uma lista completa das mudanças de cada versão, visite [a pagina do plugin](http://reitor.org/wp-plugins/maiscomentados/).
 
 == Installation ==
 
 1. Enviar o arquivo maiscomentados.php para o diretório de plugins do wordpress.
-2. Ativar o plugin.
+2. Ative o plugin.
 3. Navegue para o Painel de Configuração e personalize o widget.
-4. Se você quiser simplesmente mostrar os dados como uma lista em um post, use &lt;?php mais_comentados(1);?&gt; em seu template.
 
 == Screenshots ==
 
-1. Painel de Configuração do Plugin. :D
-2. Exemplo de uso do plugin no blog trocistas.
+1. Painel de Configuração do Plugin.
+2. Exemplo de uso do plugin.
 
 == Frequently Asked Questions ==
 
 É configuravel?
 
 Sim. Dê uma olhada nas opções do painel de configuração do widget.
-
-Duvidas/Reclamações/Sugestões:
-<a href="http://www.twitter.com/supersincero/">www.twitter.com/supersincero</a>
